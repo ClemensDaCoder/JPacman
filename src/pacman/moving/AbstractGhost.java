dd93cd57d4e6de40c5421dca61e8ef3f67d536c2
ClemensDaCoder <madincubus@hotmail.com>
2013-01-03 19:58:16 +0100

    implemented validPosition check

--- src/pacman/moving/AbstractGhost.java
+++ src/pacman/moving/AbstractGhost.java
@@ -1,12 +1,27 @@
 package pacman.moving;
 import java.awt.Color;
 
+import pacman.Game;
+
 public abstract class AbstractGhost extends MovingObject {
 
 	private Color color;
 
+	@Override
 	public void move() {
+		super.move();
+		kill();
+	}
 	
+	private void kill() {
+		//kill kill kill!
+		if (Game.getInstance().getMovingObjects().containsKey(position)) {
+			MovingObject movingObject = Game.getInstance().getMovingObjects().get(position);
+			if (movingObject instanceof PacMan) {
+				//it's over now!
+				Game.getInstance().endGame();
+			}
+		}
 	}
 	
 	public Color getColor() {
--- src/pacman/moving/RandomGhost.java
+++ src/pacman/moving/RandomGhost.java
@@ -1,23 +1,7 @@
 package pacman.moving;
 
-import pacman.Game;
 
 public class RandomGhost extends AbstractGhost {
+
 	
-	@Override
-	public void move() {
-		super.move();
-		kill();
-	}
-	
-	private void kill() {
-		//kill kill kill!
-		if (Game.getInstance().getMovingObjects().containsKey(position)) {
-			MovingObject movingObject = Game.getInstance().getMovingObjects().get(position);
-			if (movingObject instanceof PacMan) {
-				//it's over now!
-				Game.getInstance().endGame();
-			}
-		}
-	}
 }
